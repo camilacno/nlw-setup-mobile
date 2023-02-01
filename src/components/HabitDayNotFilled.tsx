c5a7f789116d0d9670ed16a7c665d1c90e3d2e14
Camila Nepomuceno <camila.cno@gmail.com>
2023-02-01 18:31:31 -0300

    New screens structure

--- src/components/HabitDayNotFilled.tsx
+++ src/components/HabitDayNotFilled.tsx
@@ -9,7 +9,7 @@ export const DAY_SIZE =
 export function HabitDayNotFilled() {
   return (
     <View
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40 cursor-not-allowed"
+      className="bg-zinc-800 rounded-lg border-2 m-1 border-zinc-800 opacity-40 cursor-not-allowed"
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
     ></View>
   )
